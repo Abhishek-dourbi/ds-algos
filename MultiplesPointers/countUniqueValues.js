42e847778288ff811be2993ce388111a0fb511cc
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2020-08-08 22:24:02 +0100

    alter method for counting unique values using multiple pointers

--- MultiplesPointers/countUniqueValues.js
+++ MultiplesPointers/countUniqueValues.js
@@ -30,22 +30,17 @@
 // 	return total;
 // }
 
+// Alter
+
 function countUniqueValues(arr) {
 	let i = 0;
-	let j = 1;
-	let res = 0;
-
-	while(j < arr.length) {
-		let sum = arr[i] === arr[j];
-		if(sum) {
-			j++;
-		} else {
-			res++;
-			i = j;
-			j++
+	for(let j = 1; j < arr.length; j++) {		
+		if(!(arr[i] === arr[j])) {
+			i++;
+			arr[i] = arr[j];
 		}
 	}
-	return res + 1;
+	return i + 1;
 }
 
 console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]));
