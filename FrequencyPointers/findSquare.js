47096b0092d439eefc67727c564a6154d1961097
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2020-08-08 16:32:41 +0100

    alter methods for finding square using frequency pointers

--- FrequencyPointers/findSquare.js
+++ FrequencyPointers/findSquare.js
@@ -18,4 +18,37 @@ function same(arr1, arr2) {
 	}
 }
 
-console.log(same([1,2,3], [4,9,2]))
+// alter
+
+// function same(arr1, arr2) {
+// 	let freqCounter = {};
+// 	for(let val of arr1) {
+// 		freqCounter[val] = (freqCounter[val] || 0) + 1;
+// 	}
+// 	for(let val of arr2) {
+// 		if(!(Math.sqrt(val) in freqCounter)) {
+// 			return false
+// 		}
+// 		freqCounter[Math.sqrt(val)] -= freqCounter[Math.sqrt(val)]
+// 	}
+// 	return true;
+// }
+
+
+// alter
+
+// function same(arr1, arr2) {
+// 	let freqCounter = {};
+// 	for(let val of arr2) {
+// 		freqCounter[val] = (freqCounter[val] || 0) + 1;
+// 	}
+// 	for(let val of arr1) {
+// 		if(!(val ** 2 in freqCounter)) {
+// 			return false
+// 		}
+// 		freqCounter[val ** 2] -= freqCounter[val ** 2]
+// 	}
+// 	return true;
+// }
+
+console.log(same([4,1,5,2,3,], [4,9,25,1,16]))
