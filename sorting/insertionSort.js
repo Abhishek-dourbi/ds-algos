bb570d48e8bb32960f0202905ea97183b96cd481
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2020-09-11 14:59:48 +0100

    sorting array of numbers using insertion sort

---
+++ sorting/insertionSort.js
@@ -0,0 +1,46 @@
+// function insertionSort(arr) {
+// 	for(let i = 1; i < arr.length; i++) {
+// 		let ele = arr[i];
+// 		for(let j = i - 1; j >= 0; j--) {
+// 			if(arr[j] > ele) {
+// 				arr[j+1] = arr[j];
+// 			}
+// 			if(j === 0 && arr[j] > ele) {
+// 				arr[j] = ele
+// 			}
+// 			if(arr[j] < ele) {
+// 				arr[j+1] = ele;
+// 				break;
+// 			}
+// 		}
+// 		console.log(arr)
+// 	}
+// 	return arr
+// }
+
+// let insertionSort = (inputArr) => {
+//     let length = inputArr.length;
+//     for (let i = 1; i < length; i++) {
+//         let key = inputArr[i];
+//         let j = i - 1;
+//         while (j >= 0 && inputArr[j] > key) {
+//             inputArr[j + 1] = inputArr[j];
+//             j = j - 1;
+//         }
+//         inputArr[j + 1] = key;
+//     }
+//     return inputArr;
+// };
+
+function insertionSort(arr) {
+	for(let i = 1; i < arr.length; i++) {
+		let ele = arr[i];
+		for(var j = i - 1; j >= 0 && arr[j] > ele; j--) {
+			arr[j+1] = arr[j];
+		}
+		arr[j+1] = ele;
+	}
+	return arr;
+}
+
+console.log(insertionSort([3,14,2,6,1,5]))
