427d89045a69f173a8cfab468d77755029fdb650
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2020-11-01 18:21:14 +0000

    remove function for a linked list from a specific index

--- ds/singlyLinkedList.js
+++ ds/singlyLinkedList.js
@@ -102,6 +102,18 @@ class SinglyLinkedList {
 		this.length++;
 		return true;
 	}
+
+	remove(index) {
+		if(index < 0 || index >= this.length) return undefined;
+		if(!index) return this.shift();
+		if(index === this.length - 1) return this.pop()
+		const pre = this.get(index - 1);
+		const curr = pre.next;
+		
+		pre.next = curr.next;
+		this.length--;
+		return curr;
+	}
 }
 
 let list = new SinglyLinkedList();
@@ -111,5 +123,6 @@ list.push("third");
 list.push("fourth");
 list.push("fifth");
 list.push("sixth");
-list.insert(4, "insert");
+// list.remove(9);
+console.log(list.remove(4));
 console.log(list);
