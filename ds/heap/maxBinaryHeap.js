8b6d6fae438aadcc77766151f22ee34758a54805
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2021-01-13 20:45:26 +0000

    adding extractMax and sinkDown method to a max binary heap

--- ds/heap/maxBinaryHeap.js
+++ ds/heap/maxBinaryHeap.js
@@ -8,6 +8,56 @@ class MaxBinaryHeap {
 		this.bubbleUp();
 	}
 
+	extractMax() {
+		let max = this.values[0];
+		let end = this.values.pop();
+		if(this.values.length) {
+			this.values[0] = end;
+			this.sinkDown();
+		}
+		return max; 
+	}
+
+	sinkDown() {
+		let i = 0;
+		const length = this.values.length;
+		let element = this.values[0];
+
+		while(true) {
+			let index;
+			let leftChildIndex = 2 * i + 1;
+			let rightChildIndex = 2 * i + 2;
+			let leftChild, rightChild;
+			let swap = null; 
+
+			if(leftChildIndex < length) {
+				leftChild = this.values[leftChildIndex];
+				if(leftChild > element) {
+					swap = leftChildIndex;
+				}
+			}
+			if(rightChildIndex < length) {
+				rightChild = this.values[rightChildIndex];
+				if(
+					(swap === null && rightChild > element) ||
+					(swap !== null && rightChild > leftChild)
+				) {
+					swap = rightChildIndex;
+				}
+			}
+
+			if(swap) {
+				let temp = this.values[i];
+				this.values[i] = this.values[swap];
+				this.values[swap] = temp ;
+				i = swap;
+				element = this.values[swap]
+			} else {
+				break;
+			}
+		}
+	}
+
 	bubbleUp() {
 		let index = this.values.length - 1;
 		let parentIndex = Math.floor((index - 1) / 2);
@@ -31,5 +81,6 @@ obj.insert(18);
 obj.insert(27);
 obj.insert(12);
 obj.insert(55);
+obj.extractMax();
 
 console.log(obj.values);
