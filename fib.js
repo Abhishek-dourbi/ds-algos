791bdf8816281dee1e0d63f817222f73c3c38717
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2021-02-13 21:48:53 +0000

    fibonacci sequence using tabulation

--- fib.js
+++ fib.js
@@ -1,17 +1,17 @@
-// function fib(n) {
-// 	if(n <= 2) return 1;
-// 	return fib(n - 1) + fib(n - 2);
-// }
+function fib_rec(n) {
+	if(n <= 2) return 1;
+	return fib_rec(n - 1) + fib_rec(n - 2);
+}
 
 // TIme complexity
 // O(2^n);
 
 // enter memoization
 
-function fib(n, memo = []) {
+function fib_memo(n, memo = []) {
 	if(memo[n]) return memo[n];
 	if(n <= 2) return 1;
-	const res = fib(n-1, memo) + fib(n-2, memo);
+	const res = fib_memo(n-1, memo) + fib_memo(n-2, memo);
 	memo[n] = res;
 	return res;
 }
@@ -19,4 +19,19 @@ function fib(n, memo = []) {
 // TIme complexity
 // O(n);
 
-console.log(fib(10))
+// enter tabulation
+
+function fib_table(n) {
+	const fibNums = [0, 1, 1];
+
+	for(let i = 3; i <= n; i++) {
+		fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+	}
+
+	return fibNums[n];
+}
+
+// TIme complexity
+// O(n);
+
+console.log(fib_table(10000))
