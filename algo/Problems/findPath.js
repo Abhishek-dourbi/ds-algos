cfbf9ffad57c20510526728a8accbf57b0310c62
Abhishek-dourbi <abhishekdourbi98@gmail.com>
2021-03-18 17:44:07 +0000

    returning value from the object passed according to dot operator

---
+++ algo/Problems/findPath.js
@@ -0,0 +1,41 @@
+/*
+- Write method findPath
+- Should take two params:
+    - object
+    - keys separated by dots as string
+- Return value if it exists at that path inside the object, else return undefined
+*/
+
+var obj = {
+    a: {
+        b: {
+            c: 12,
+            j: false
+        },
+        k: null
+    }
+};
+
+function findPath(obj, val) {
+    let valArr = val.split('.');
+    let res = obj;
+    for(let i = 0; i < valArr.length; i++) {
+        if(typeof(res[valArr[i]]) !== "undefined") {
+            res = res[valArr[i]];
+        } else {
+            res = undefined;
+            break;
+        }
+    }
+    return res;
+}
+
+console.log(findPath(obj, 'a.b.c')); // 12
+console.log(findPath(obj, 'a.b')); // {c: 12, j: false}
+console.log(findPath(obj, 'a.b.d')); // undefined
+console.log(findPath(obj, 'a.c')); // undefined
+console.log(findPath(obj, 'a.b.c.d')); // undefined
+console.log(findPath(obj, 'a.b.c.d.e')); // undefined
+console.log(findPath(obj, 'a.b.j')); //false
+console.log(findPath(obj, 'a.b.j.k')); //undefined
+console.log(findPath(obj, 'a.k')); //null
